ecdba554f1aa9a2e2acc8124daf11633e6042d65
Felix Koch <mail@felixkoch.de>
2019-06-09 18:42:08 +0000

    Handlers

--- Edge.js
+++ Edge.js
@@ -1,17 +1,45 @@
 const sdpTransform = require('sdp-transform');
 const sdpCommonUtils = require('mediasoup-client/lib/handlers/sdp/commonUtils');
+const Logger = require('mediasoup-client/lib/Logger');
+const EnhancedEventEmitter = require('mediasoup-client/lib/EnhancedEventEmitter');
+
+const logger = new Logger('Edge');
+
+class Handler extends EnhancedEventEmitter
+{
+	constructor(
+		{
+			iceParameters,
+			iceCandidates,
+			dtlsParameters,
+			iceServers,
+			iceTransportPolicy,
+			proprietaryConstraints
+		}
+	)
+	{
+		super(logger);
+	}
+}
+
+class SendHandler extends Handler
+{
+	constructor(data)
+	{
+		super(data);
+	}
+}
+
+class RecvHandler extends Handler
+{
+	constructor(data)
+	{
+		super(data);
+
+	}
+}
+
 class Edge {
-
-    constructor()
-    {
-        console.log('inst');
-    }
-    
-    test()
-    {
-        this.test = 'Edge'
-    }
-
     
 	static async getNativeRtpCapabilities()
 	{
@@ -53,7 +81,66 @@ class Edge {
 
 			throw error;
 		}
-    }
+	}
+	
+	constructor(
+		{
+			direction,
+			iceParameters,
+			iceCandidates,
+			dtlsParameters,
+			iceServers,
+			iceTransportPolicy,
+			proprietaryConstraints,
+			extendedRtpCapabilities
+		}
+	)
+	{
+		logger.debug('constructor() [direction:%s]', direction);
+
+		switch (direction)
+		{
+			case 'send':
+			{
+				const sendingRtpParametersByKind =
+				{
+					audio : ortc.getSendingRtpParameters('audio', extendedRtpCapabilities),
+					video : ortc.getSendingRtpParameters('video', extendedRtpCapabilities)
+				};
+
+				const sendingRemoteRtpParametersByKind =
+				{
+					audio : ortc.getSendingRemoteRtpParameters('audio', extendedRtpCapabilities),
+					video : ortc.getSendingRemoteRtpParameters('video', extendedRtpCapabilities)
+				};
+
+				return new SendHandler(
+					{
+						iceParameters,
+						iceCandidates,
+						dtlsParameters,
+						iceServers,
+						iceTransportPolicy,
+						proprietaryConstraints,
+						sendingRtpParametersByKind,
+						sendingRemoteRtpParametersByKind
+					});
+			}
+
+			case 'recv':
+			{
+				return new RecvHandler(
+					{
+						iceParameters,
+						iceCandidates,
+						dtlsParameters,
+						iceServers,
+						iceTransportPolicy,
+						proprietaryConstraints
+					});
+			}
+		}
+	}
 
 }
 
